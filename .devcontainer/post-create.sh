21f9c73bda96e80eca39392129ba99abe6149242
Dominique Broeglin <dominique.broeglin@microsoft.com>
2024-12-17 22:10:39 +0000

    Including adr-tool in DevContainer setup

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+
+# Install uv, see https://astral.sh for additional information
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync
+
+# Install adr-tools, see https://github.com/npryce/adr-tools
+git clone https://github.com/npryce/adr-tools.git --depth 1 $HOME/.adr-tools
+echo 'export PATH=$PATH:$HOME/.adr-tools/src' >> $HOME/.bashrc
