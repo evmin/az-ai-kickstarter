995207b323e19f41ba6ea664a912d77da7bbdbc2
Dominique Broeglin <dominique.broeglin@microsoft.com>
2025-01-02 19:30:13 +0100

    Removing uv sync for l100 solution

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,9 +6,6 @@ sudo apt update
 
 # Install uv, see https://astral.sh for additional information
 curl -LsSf https://astral.sh/uv/install.sh | sh
-echo 'export UV_LINK_MODE=copy' >> $HOME/.bashrc
-source $HOME/.bashrc
-uv sync --all-packages
 
 # Install adr-tools, see https://github.com/npryce/adr-tools
 git clone https://github.com/npryce/adr-tools.git --depth 1 $HOME/.adr-tools
